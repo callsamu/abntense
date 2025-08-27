36d5995d5c3243a1af1fcea35b9248fb028a899d
callsamu <samuelbree@gmail.com>
2025-08-27 19:26:00 -0300

    feat: setup VoltUI

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,11 +1,16 @@
 import { App } from 'vue'
 import * as components from './components'
+import PrimeVue from 'primevue/config';
 
 function install (app: App) {
   for (const key in components) {
     // @ts-expect-error
     app.component(key, components[key])
   }
+
+  app.use(PrimeVue, {
+    unstyled: true
+  });
 }
 
 export default { install }
