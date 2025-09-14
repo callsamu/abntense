384eec2b4fee03018d7a734398368733360bc94e
callsamu <samuelbree@gmail.com>
2025-09-13 21:21:22 -0300

    feat: configure cypress for PrimeVue

--- packages/ui/cypress/support/component.ts
+++ packages/ui/cypress/support/component.ts
@@ -1,5 +1,25 @@
 /// <reference types="../component" />
 import './commands'
+import '@/styles/app.css'
 import { mount } from 'cypress/vue'
+import PrimeVue from 'primevue/config'
 
-Cypress.Commands.add('mount', mount)
+
+Cypress.Commands.add('mount', (component, ...args) => {
+    // @ts-ignore
+    args.global = args.global || {}
+    // @ts-ignore
+    args.global.plugins = args.global.plugins || []
+    // @ts-ignore
+    args.global.plugins.push({
+        // @ts-ignore
+        install(app) {
+            app.use(PrimeVue, {
+                unstyled: true
+            });
+        }
+    })
+
+    // @ts-ignore
+    return mount(component, args);
+});
