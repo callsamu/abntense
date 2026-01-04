50a560423ae29d4c83501db163a84a744a77eb99
callsamu <samuelbree@gmail.com>
2026-01-04 09:15:06 -0300

    fix(editor): update test app editor usage

--- packages/editor/src/App.vue
+++ packages/editor/src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
     import Editor from '../lib/Editor.vue';
+    import useEditor from '../lib/useEditor';
+
+    const editor = useEditor({});
 </script>
 
 <template>
     <div class="w-full h-screen flex justify-center items-center">
-        <div class="w-1/2 h-full bg-neutral-800 text-neutral-100 flex p-2 px-8">
-            <Editor />
+        <div v-if="editor" class="w-1/2 h-full bg-neutral-800 text-neutral-100 flex p-2 px-8">
+            <Editor :editor="editor" />
         </div>
     </div>
 </template>
